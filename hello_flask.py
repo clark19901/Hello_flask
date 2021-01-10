43995ff6b2c17c72a62e6520b8101756652def9c
Clark <clark1990@me.com>
2021-01-10 16:10:44 +0800

    use Jinja2

--- hello_flask.py
+++ hello_flask.py
@@ -1,9 +1,13 @@
-from flask import Flask
+
+from flask import abort  #处理错误
+from flask import redirect #重定向
+from flask_script import Manager
+from flask import Flask, render_template #Jinja2模板引擎
 
 app = Flask(__name__)
 
-#代码示例 route() 装饰器把一个函数绑定到对应的 URL 上。
-#@app.route('/')
+# 代码示例 route() 装饰器把一个函数绑定到对应的 URL 上。
+# @app.route('/')
 # def index():
 #     return 'Index Page'
 
@@ -11,16 +15,33 @@ app = Flask(__name__)
 # def hello():
 #     return 'Hello World'
 
+@app.route('/re')
+def redi():
+    return redirect('http://www.baidu.com')
+#重定向 告诉浏览器一个新地址用以加载新页面
+
+# @app.route('/user/<id>')
+# def get_user(id):
+#     user = load_user(id)
+#     if not user:
+#         abort(404)
+#     return '<h1>Hello, %s</h1>' % user.name
+
+
 @app.route('/')
-def hello_world():
-    return "<h1>Hello Wolrd!<h1>"
+def index():
+    return render_template('index.html')
 
-#添加了一个动态路由。访问这个地址时，你会看到一则针对个人的欢迎消息。
+# 添加了一个动态路由。访问这个地址时，你会看到一则针对个人的欢迎消息。
+
+
+# @app.route('/user/<name>')
+# def user(name):
+#     return '<h1>Hello, %s!</h1>' % name
+
 @app.route('/user/<name>')
-
 def user(name):
-    return '<h1>Hello, %s!</h1>' % name
-
+    return render_template('user.html', name=name)
 
 if __name__ == "__main__":
     app.debug = True
